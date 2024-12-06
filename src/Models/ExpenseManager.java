b795f557c09e6417b80e7eada66eb5017f5d2dbd
Divya Bhutani <divyabhutani33@gmail.com>
2024-12-05 20:13:22 -0500

    Update ExpenseManager.java

--- src/Models/ExpenseManager.java
+++ src/Models/ExpenseManager.java
@@ -1,1 +1,21 @@
+package Services;
 
+import java.util.List;
+import java.util.Map;
+
+import Models.Expenses;
+import Models.User;
+
+public class ExpenseManager{
+  List<Expense> expenseDataStore;
+  Map<String, User> userDataStore;
+  {
+    u1 : {
+      u2 : 250
+        u3 : 250
+        u4 : 250
+        }
+  }
+
+}
+}
